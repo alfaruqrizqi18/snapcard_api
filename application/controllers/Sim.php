971be28cd3a2a7e677089b88276026f60a4edd7c
Rizqi Alfaruq <alfaruqrizqi18@gmail.com>
2020-08-04 11:01:18 +0700

    progress

--- application/controllers/Sim.php
+++ application/controllers/Sim.php
@@ -1,18 +1,79 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Sim extends CI_Controller {
+class Sim extends CI_Controller
+{
 
-	public function sim_card()
-	{
-        
-        $query = $this->db->query('SELECT * FROM sim_card')->result_array();
+    public function sim_card()
+    {
+        $query = $this->db->query('SELECT * FROM sim_card ORDER BY id DESC')->result_array();
         $to_json = array(
-            'status_code' => 200,
             'message' => "success",
-            "data" => $query
+            'row' => count($query),
+            "data" => $query == null ? null : $query
         );
-		$this->output->set_content_type('application/json')
-        ->set_output(json_encode($to_json));
-	}
+        $this->output->set_content_type('application/json')
+            ->set_output(json_encode($to_json));
+    }
+
+    public function sim_card_plus()
+    {
+        $query = $this->db->query('SELECT * FROM sim_card_plus ORDER BY id DESC')->result_array();
+        $to_json = array(
+            'message' => "success",
+            'row' => count($query),
+            "data" => $query == null ? null : $query
+        );
+        $this->output->set_content_type('application/json')
+            ->set_output(json_encode($to_json));
+    }
+
+
+    public function create_sim_card()
+    {
+        $input_sim_card_barcode = $this->input->post('sim_card_barcode');
+        $to_json = array();
+        $query = $this->db->query("SELECT * FROM sim_card WHERE sim_card_barcode = '$input_sim_card_barcode'")->result_array();
+        if ($query != null) {
+            $to_json = array(
+                'message' => "duplicate"
+            );
+        } else {
+            $this->db->insert('sim_card', array('sim_card_barcode' => $input_sim_card_barcode));
+            $to_json = array(
+                'message' => "success"
+            );
+        }
+        $this->output->set_content_type('application/json')
+            ->set_output(json_encode($to_json));
+    }
+
+    public function create_sim_card_plus()
+    {
+        $input_sim_card_plus_barcode = $this->input->post('sim_card_plus_barcode');
+        $input_physical_voucher = $this->input->post('physical_voucher');
+        $to_json = array();
+        $query = $this->db->query(
+            "SELECT * 
+            FROM sim_card_plus 
+            WHERE sim_card_plus_barcode = '$input_sim_card_plus_barcode'
+            AND physical_voucher = '$input_physical_voucher'
+            "
+        )->result_array();
+        if ($query != null) {
+            $to_json = array(
+                'message' => "duplicate"
+            );
+        } else {
+            $this->db->insert('sim_card_plus', array(
+                'sim_card_plus_barcode' => $input_sim_card_plus_barcode,
+                'physical_voucher' => $input_physical_voucher
+            ));
+            $to_json = array(
+                'message' => "success"
+            );
+        }
+        $this->output->set_content_type('application/json')
+            ->set_output(json_encode($to_json));
+    }
 }
--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,18 +1,31 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Users extends CI_Controller {
+class Users extends CI_Controller
+{
 
-	public function index()
-	{
-        
-        $query = $this->db->query('SELECT * FROM sim_card')->result_array();
-        $to_json = array(
-            'status_code' => 200,
-            'message' => "success",
-            "data" => $query
-        );
-		$this->output->set_content_type('application/json')
-        ->set_output(json_encode($to_json));
-	}
+    public function login()
+    {
+        $input_username = $this->input->post('username');
+        $input_password = sha1($this->input->post('password'));
+        $to_json = array();
+        $query = $this->db->query(
+            "SELECT * 
+            FROM users 
+            WHERE username = '$input_username'
+            AND password = '$input_password'
+            "
+        )->result_array();
+        if ($query != null) {
+            $to_json = array(
+                'message' => "success",
+            );
+        } else {
+            $to_json = array(
+                'message' => "failed",
+            );
+        }
+        $this->output->set_content_type('application/json')
+            ->set_output(json_encode($to_json));
+    }
 }
